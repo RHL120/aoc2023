d03c10a2788c90ae7aa3ffdc541485ca2cd87ca2
RHL120 <rhl120@protonmail.com>
2023-12-02 07:42:53 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use aoc2023::sols::*;
+use std::env;
+use std::fs;
+
+fn parse_args() -> Option<(usize, usize, String)> {
+    let args: Vec<String> = env::args().collect();
+    let day: usize = args.get(1).and_then(|x| x.parse().ok())?;
+    let part: usize = args.get(2).and_then(|x| x.parse().ok())?;
+    let input: &String = args.get(3)?;
+    if day == 0 || part == 0 {
+        None
+    } else {
+        Some((day, part, input.to_string()))
+    }
+}
+
+const SOLVERS: &[&[fn(&str) -> Result<String, String>]] = &[&[]];
+fn main() -> Result<(), String> {
+    let usage_string = "Usage: ./aoc2023 <day> <part> <input>";
+    let args: Vec<String> = env::args().collect();
+    let (day, part, input_path) = parse_args().ok_or(usage_string)?;
+    let input =
+        fs::read_to_string(&input_path).map_err(|_| format!("Failed to read {input_path}"))?;
+    let &day_solutions = SOLVERS
+        .get(day - 1)
+        .ok_or_else(|| format!("Failed to get day {day}"))?;
+    let solution = day_solutions
+        .get(part - 1)
+        .ok_or_else(|| format!("Failed to get part {part}"))?;
+    println!(
+        "The solution for day {day} part {part} is:\n{}",
+        solution(&input)?
+    );
+    Ok(())
+}
