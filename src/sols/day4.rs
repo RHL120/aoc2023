034e2964536012e8651f2c071779a89a811e7681
RHL120 <rhl120@protonmail.com>
2023-12-04 07:24:04 +0100

    Solve day 4 part 2

--- src/sols/day4.rs
+++ src/sols/day4.rs
@@ -1,4 +1,5 @@
 use crate::utils::parser;
+use std::collections::HashMap;
 fn parse_card(s: &str) -> Option<((u32, Vec<u32>, Vec<u32>), &'_ str)> {
     let (_, s) = parser::parse_const(s, "Card")?;
     let (id, s) = parser::parse_unsigned_int(s.trim())?;
@@ -26,7 +27,7 @@ fn parse_game(s: &str) -> Option<Vec<(u32, Vec<u32>, Vec<u32>)>> {
         .collect()
 }
 
-fn num_winning(w: &Vec<u32>, m: &Vec<u32>) -> usize {
+fn num_matching(w: &Vec<u32>, m: &Vec<u32>) -> usize {
     m.iter().filter(|&x| w.contains(x)).count()
 }
 
@@ -35,7 +36,7 @@ pub fn part1(input: &str) -> Result<String, String> {
     let res = game
         .iter()
         .map(|(id, v1, v2)| {
-            let num = num_winning(v1, v2);
+            let num = num_matching(v1, v2);
             if num == 0 {
                 0
             } else {
@@ -46,6 +47,45 @@ pub fn part1(input: &str) -> Result<String, String> {
     Ok(res.to_string())
 }
 
+fn matchings(game: &Vec<(u32, Vec<u32>, Vec<u32>)>) -> HashMap<usize, usize> {
+    game.iter()
+        .map(|(id, v1, v2)| ((*id as usize), num_matching(v1, v2)))
+        .collect()
+}
+
+fn new_instances(card: usize, mats: &HashMap<usize, usize>) -> HashMap<usize, usize> {
+    let mut ret = HashMap::new();
+    for mat in 1..=mats[&card] {
+        let ni = card + mat;
+        match ret.get(&ni) {
+            Some(x) => ret.insert(ni, x + 1),
+            None => ret.insert(ni, 1),
+        };
+        let nis = new_instances(ni, mats);
+        for (k, v) in nis {
+            match ret.get(&k) {
+                Some(x) => ret.insert(k, x + v),
+                None => ret.insert(k, v),
+            };
+        }
+    }
+    ret
+}
+
 pub fn part2(input: &str) -> Result<String, String> {
-    todo!()
+    let game = parse_game(input).ok_or("Failed to parse game")?;
+    let matches = matchings(&game);
+    let mut instances: HashMap<usize, usize> =
+        game.iter().map(|(id, _, _)| (*id as usize, 1)).collect();
+    for card in matches.keys() {
+        let ni = new_instances(*card, &matches);
+        for (k, v) in ni {
+            match instances.get(&k) {
+                Some(x) => instances.insert(k, x + v),
+                None => instances.insert(k, v),
+            };
+        }
+    }
+    let res = instances.iter().map(|(_, x)| *x).sum::<usize>();
+    Ok(res.to_string())
 }
