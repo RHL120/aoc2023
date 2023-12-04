788c9437f51560dc3ed5f61df0bf9943792000a1
RHL120 <rhl120@protonmail.com>
2023-12-04 13:36:18 +0100

    Add some tests for day 4

--- src/sols/day4.rs
+++ src/sols/day4.rs
@@ -35,7 +35,7 @@ pub fn part1(input: &str) -> Result<String, String> {
     let game = parse_game(input).ok_or("Failed to parse game")?;
     let res = game
         .iter()
-        .map(|(id, v1, v2)| {
+        .map(|(_, v1, v2)| {
             let num = num_matching(v1, v2);
             if num == 0 {
                 0
@@ -88,4 +88,23 @@ pub fn part2(input: &str) -> Result<String, String> {
     }
     let res = instances.iter().map(|(_, x)| *x).sum::<usize>();
     Ok(res.to_string())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const INPUT: &str = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(INPUT).unwrap(), "13");
+    }
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(INPUT).unwrap(), "30");
+    }
 }
