43843cb86daf472cc6e86edc1fe32fd50572030f
RHL120 <rhl120@protonmail.com>
2023-12-08 19:06:52 +0100

    Write math::lcm in terms of math::gcd

--- src/utils/math.rs
+++ src/utils/math.rs
@@ -8,14 +8,8 @@ pub fn gcd(mut n: u128, mut m: u128) -> u128 {
     }
     n
 }
-pub fn lcm(nums: &[u128]) -> Option<u128> {
-    let &mx = nums.iter().max()?;
-    let prod = nums.iter().product::<u128>();
-    nums.iter().all(|&x| x != 0).then(|| {
-        (mx..=prod)
-            .find(|&n| nums.iter().all(|&m| n % m == 0))
-            .unwrap()
-    })
+pub fn lcm(n: u128, m: u128) -> u128 {
+    n * m / gcd(n, m)
 }
 
 #[cfg(test)]
@@ -23,5 +17,8 @@ mod tests {
     use super::*;
     fn test_gcd() {
         assert_eq!(gcd(34, 134), 2)
+    }
+    fn test_lcm() {
+        assert_eq!(gcd(3, 2), 6)
     }
 }
